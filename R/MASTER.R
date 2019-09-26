383caaf91d0d44b9c6f7d5e4200582009dd76ba5
vbarbarossa <vbarbarossa@science.ru.nl>
2019-09-26 11:38:22 +0200

    update for cartesius

--- R/MASTER.R
+++ R/MASTER.R
@@ -10,7 +10,7 @@ dir_tabs <- dir_('tabs/')
 dir_proc <- dir_('proc/')
 
 # packages needed
-library(sf); library(foreach); library(rfishbase); library(data.table); library(ggplot2); library(viridis)
+library(sf); library(foreach); library(rfishbase); library(data.table); library(ggplot2); library(viridis); library(dplyr)
 # not fully loaded: foreign, rnaturalearth,
 
 # location of hydrobasins shapefiles
@@ -20,10 +20,14 @@ dir_hybas12 <- '/vol/milkunarc/vbarbarossa/data/HydroBASINS/global_lev12'
 
 # IUCN data
 # data is freely accessible @ https://www.iucnredlist.org/resources/spatial-data-download
-dir_iucn_fish <- '/vol/milkunarc/vbarbarossa/data/IUCN/FW_FISH_20181113'
-file_iucn_fish1 <- '/vol/milkunarc/vbarbarossa/data/IUCN/FW_FISH_20181113/FW_FISH_PART_1.shp'
-file_iucn_fish2 <- '/vol/milkunarc/vbarbarossa/data/IUCN/FW_FISH_20181113/FW_FISH_PART_2.shp'
+dir_iucn_fish <- '../data/IUCN/FW_FISH_20181113'
+file_iucn_fish1 <- '../data/IUCN/FW_FISH_20181113/FW_FISH_PART_1.shp'
+file_iucn_fish2 <- '../data/IUCN/FW_FISH_20181113/FW_FISH_PART_2.shp'
 file_iucn_habitat_type <- 'data/iucn_habitat_type.csv' # from fishsuit
+
+# custom ranges data
+file_custom_ranges <- '../occ2range4fish/out/custom_ranges_poly.gpkg'
+file_custom_ranges_habitat_type <- '../occ2range4fish/out/custom_ranges_habitatFishbase.csv'
 
 # dams data
 # data is freely accessible @
