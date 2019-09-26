fab13f96c623a75f32dafa81b9fa1cfd34985477
vbarbarossa <vbarbarossa@science.ru.nl>
2019-09-26 15:33:11 +0200

    dynamic occ threshold

--- R/preprocess/extract_customRanges2hybas12.R
+++ R/preprocess/extract_customRanges2hybas12.R
@@ -19,29 +19,40 @@ if(file.exists('proc/hybas12_points_nolakes.gpkg')){
 # load IUCN data
 iucn <- rbind(read_sf(file_iucn_fish1),read_sf(file_iucn_fish2))
 
-# load species shapefile and filter
-sp <- read_sf(file_custom_ranges) %>%
-  filter(no_occ >= min_no_occ) %>% #filter out based on no_occ threshold
-  # filter(!name %in% unique(as.character(habitat$name[habitat$OnlyLake == -1]))) %>% # filter out exclusively lentic species <<< should be done later on to have a full comparison with Tedesco data
-  filter(!name %in% unique(as.character(iucn$binomial))) # filter out species already covered in the IUCN dataset
-
-# reference to hydrobasins level 12
-lst <- st_contains(sp,points,sparse = T)
-# lst is a sparse matrix where each entry is a row of sp and contains a list of hybas12 points falling within that species polygos
-
-# make database where each entry is a hybas ID and 
-# loop through the species
-# for each species, create a table with hybasID and species id_no
-
-tab <- lapply(seq_along(lst),function(i){
-  hb <- points$HYBAS_ID[lst[[i]]]
-  if(length(hb) > 0){
-    return(
-      data.frame(HYBAS_ID = hb,
-                 binomial = sp$name[i])
-    )
+# loop through multiple thresholds
+for(th in c(0,1,2,5,10,20,50,100)){
+  min_no_occ <- th
+  
+  # load species shapefile and filter
+  sp <- read_sf(file_custom_ranges) %>%
+    filter(no_occ >= min_no_occ) %>% #filter out based on no_occ threshold
+    # filter(!name %in% unique(as.character(habitat$name[habitat$OnlyLake == -1]))) %>% # filter out exclusively lentic species <<< should be done later on to have a full comparison with Tedesco data
+    filter(!name %in% unique(as.character(iucn$binomial))) # filter out species already covered in the IUCN dataset
+  
+  # reference to hydrobasins level 12
+  lst <- st_contains(sp,points,sparse = T)
+  # lst is a sparse matrix where each entry is a row of sp and contains a list of hybas12 points falling within that species polygos
+  
+  # make database where each entry is a hybas ID and 
+  # loop through the species
+  # for each species, create a table with hybasID and species id_no
+  
+  tab <- lapply(seq_along(lst),function(i){
+    hb <- points$HYBAS_ID[lst[[i]]]
+    if(length(hb) > 0){
+      return(
+        data.frame(HYBAS_ID = hb,
+                   binomial = sp$name[i])
+      )
+    }
   }
+  ) %>% do.call('rbind',.) %>% distinct()
+  
+  write.csv(tab,paste0('proc/hybas12_fish_custom_ranges_occth',min_no_occ,'.csv'),row.names = F)
+  
+  cat('\n\nMinimum no. occurrences per species: ',min_no_occ,
+      '\nTotal no. of extra species referenced on hybas12: ',length(unique(tab$binomial)))
+  
 }
-) %>% do.call('rbind',.) %>% distinct()
 
-write.csv(tab,paste0('proc/hybas12_fish_custom_ranges_occth',min_no_occ,'.csv'),row.names = F)
+
