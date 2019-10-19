bb99131837fabb6f7f54cb1f90e6f155b18815e8
vbarbarossa <vbarbarossa@science.ru.nl>
2019-10-19 17:57:17 +0200

    update for brazil analysis

---
+++ sh/calculate_CI_BRA.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH --time=1-00:00:00
+#SBATCH --output=sh/calculate_CI_BRA_%a.out
+#SBATCH --mail-type=END
+#SBATCH --mail-user=vbarbarossa@science.ru.nl
+
+module load 2019
+module load R/3.5.1-foss-2018b
+
+Rscript R/calculate_CI_BRA.R
